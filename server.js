02d3c0ca45c0171f367587999a3595bd1e85ecc0
Rattanak Leng <rleng@live.com>
2020-12-16 15:04:15 -0800

    commit to fix heroku problem

--- server.js
+++ server.js
@@ -37,6 +37,10 @@ let waitlist = [];
 //     res.json(path.join(__dirname, "index.html"));
 // });
 
+app.get("/", function (req, res) {
+    res.json(path.join(__dirname, "index.html"));
+});
+
 app.get("/tables", function (req, res) {
     res.sendFile(path.join(__dirname, "tables.html"));
 });
