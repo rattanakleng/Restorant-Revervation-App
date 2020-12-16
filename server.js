0726f59ecd61527dab5bd6a581cd83fd49c769d2
Rattanak Leng <rleng@live.com>
2020-12-16 15:21:28 -0800

    fix heroku

--- server.js
+++ server.js
@@ -37,9 +37,9 @@ let waitlist = [];
 //     res.json(path.join(__dirname, "index.html"));
 // });
 
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
+// app.get("/", function (req, res) {
+//     res.sendFile(path.join(__dirname, "index.html"));
+// });
 
 app.get("/tables", function (req, res) {
     res.sendFile(path.join(__dirname, "tables.html"));
