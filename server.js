66f0f4aa1e12fbee85f9bbee8e81691e31ebbe5b
Rattanak Leng <rleng@live.com>
2020-12-16 14:42:37 -0800

    commit to pull

--- server.js
+++ server.js
@@ -33,49 +33,49 @@ let waitlist = [];
 // require("./routes/htmlRoutes")(app);
 
 // app.post("/api/tables", )
-app.get("/", function(req, res) {
-    res.json(path.join(__dirname, "index.html"));
-});
+// app.get("/", function(req, res) {
+//     res.json(path.join(__dirname, "index.html"));
+// });
 
-app.get("/tables", function(req, res) {
+app.get("/tables", function (req, res) {
     res.sendFile(path.join(__dirname, "tables.html"));
 });
 
-app.get("/reserve", function(req, res) {
+app.get("/reserve", function (req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
-app.get("/api/tables", function(req, res) {
+app.get("/api/tables", function (req, res) {
     return res.json(tables);
 });
 
-app.get("/api/waitlist", function(req, res) {
+app.get("/api/waitlist", function (req, res) {
     return res.json(waitlist);
 });
 
-app.post("/api/tables", function(req, res) {
+app.post("/api/tables", function (req, res) {
     const newRes = req.body;
-    
+
     if (tables.length < 5) {
         tables.push(newRes);
-        
+
     } else {
         waitlist.push(newRes);
     }
-    
+
 });
 
-app.post("/api/clearAPI", function(req, res) {
+app.post("/api/clearAPI", function (req, res) {
 
-    
+
     tables = [];
     waitlist = [];
-    
+
 });
 
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
 // Listener
-app.listen( PORT )
+app.listen(PORT)
